6b6db6692dbedc6090a5d65dfa55744673b55c26
Acer <Acer@Knicolas>
2025-12-18 12:19:37 +0800

    the final, final codes Signed-off-by:Acer <Acer@Knicolas>

--- src/check_in/CheckInPanel.java
+++ src/check_in/CheckInPanel.java
@@ -16,7 +16,7 @@ import javax.swing.JOptionPane;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader; // what does this do?
+import java.io.FileReader;
 import java.io.FileWriter;
 
 
